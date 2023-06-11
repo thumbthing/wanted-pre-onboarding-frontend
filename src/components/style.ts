12f678eb326c7dd263e28c1dacd7faeb38551a93
thumbthing <ilscc0902@gmail.com>
2023-06-12 04:19:10 +0900

    design: 버튼 텍스트 스타일 추가

--- src/components/style.ts
+++ src/components/style.ts
@@ -3,6 +3,7 @@ import { styled } from 'styled-components';
 export const StyledButton = styled.input`
 	display: flex;
 	justify-content: space-evenly;
+	color: black;
 `;
 
 export const StyledUserInformationBox = styled.div`
