b841dbe6101b24075e8270b4e559d3411a4bc497
thumbthing <ilscc0902@gmail.com>
2023-06-12 00:23:29 +0900

    chore: git 설정 변경

---
+++ src/components/sign/SignInButton.tsx
@@ -0,0 +1,58 @@
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import { sign } from "../../request/Api";
+
+interface signInButtonProps {
+  id: string;
+  password: string;
+}
+
+const SignInButton = ({ id, password }: signInButtonProps) => {
+  const idValidation: boolean = id.includes("@");
+  const passwordValidation: number = password.length;
+  const request: string = "signin";
+  const navigate = useNavigate();
+
+  const handleSignIn = useCallback(async () => {
+    try {
+      const response = await sign(id, password, request);
+      const data = response?.data;
+
+      const key: string = Object.keys(data)[0];
+      const accessToken: string = Object.values(data)[0] as string;
+      localStorage.setItem(key, accessToken);
+
+      navigate("/todo");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [id, password, request, navigate]);
+
+  if (idValidation && passwordValidation > 7) {
+    return (
+      <StyledButton
+        data-testid='signin-button'
+        type='button'
+        value={"로그인"}
+        onClick={handleSignIn}
+      />
+    );
+  } else {
+    return (
+      <StyledButton
+        data-testid='signin-button'
+        type='button'
+        value={`아이디 조건 : @ 포함되어야 합니다 \n 비밀번호 조건: 8자 이상이어야 합니다`}
+        disabled
+      />
+    );
+  }
+};
+
+export default SignInButton;
+
+const StyledButton = styled.input`
+  display: flex;
+  justify-content: space-evenly;
+`;
