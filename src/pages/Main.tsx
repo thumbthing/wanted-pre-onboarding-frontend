5bed45d0d69ca5a38395c256c45f024b073cd82d
thumbthing <ilscc0902@gmail.com>
2023-08-04 02:15:00 +0900

    feat: 메인 페이지 기초 header 생성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to={`signin`}>로그인</Link>
-        </li>
-        <li>
-          <Link to={`signup`}>회원가입</Link>
-        </li>
-        <li>
-          <Link to={`todo`}>TodoList</Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <header>원티드 프리온보딩 프론트엔드 - 선발과제</header>
+      <div>
+        <ul>
+          <li>
+            <Link to={`signin`}>로그인</Link>
+          </li>
+          <li>
+            <Link to={`signup`}>회원가입</Link>
+          </li>
+          <li>
+            <Link to={`todo`}>TodoList</Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
