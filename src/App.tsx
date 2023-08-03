95a69a2c4fb26b880e6a101b0913c4f45bce3666
thumbthing <ilscc0902@gmail.com>
2023-08-04 00:52:40 +0900

    feat: 각 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to={`signin`}>로그인</Link>
+          </li>
+          <li>
+            <Link to={`signup`}>회원가입</Link>
+          </li>
+          <li>
+            <Link to={`todo`}>TodoList</Link>
+          </li>
+        </ul>
+      </div>
+    </BrowserRouter>
   );
 }
 
