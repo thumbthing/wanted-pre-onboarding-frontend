d9b7e541e6a6a68a87e34901e5760826c1fd03a6
thumbthing <ilscc0902@gmail.com>
2023-08-05 02:51:49 +0900

    feat: todo page 토큰여부에 따른 페이지 이동

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Todo = () => {
+  const [accessToken] = useState(localStorage.getItem("access_token"));
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate("/signin");
+    }
+  }, [accessToken, navigate]);
+
   return <div>Todo</div>;
 };
 
