6f53350fa7a0394f26d9d8ccbb4ca613d03b9592
thumbthing <ilscc0902@gmail.com>
2023-08-05 01:29:17 +0900

    feat: 회원가입 요청 api 추가

---
+++ src/api/auth/auth.ts
@@ -0,0 +1,21 @@
+import instance from "../instance";
+
+interface UserInformation {
+  email: string;
+  password: string;
+}
+
+export const signUp = async ({ email, password }: UserInformation) => {
+  const data = {
+    email,
+    password,
+  };
+
+  try {
+    const response = await instance.post(`/auth/signup`, data);
+    return response;
+  } catch (error) {
+    console.log("sign up error : ", error);
+    return null;
+  }
+};
