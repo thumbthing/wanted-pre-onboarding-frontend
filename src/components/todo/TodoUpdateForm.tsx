09b390c0007ea68c6ce6cd6ddab797b6199a369e
thumbthing <ilscc0902@gmail.com>
2023-08-06 21:42:01 +0900

    feat: 수정폼의 각 input 기능 추가

--- src/components/todo/TodoUpdateForm.tsx
+++ src/components/todo/TodoUpdateForm.tsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { TodosProps } from "../../pages/Todo";
 
-const TodoUpdateForm = () => {
+interface TodoUpdateFormProps {
+  todos: TodosProps;
+  handleIsEditing: (todoId: number) => void;
+}
+
+const TodoUpdateForm = ({ todos, handleIsEditing }: TodoUpdateFormProps) => {
   return (
     <>
       <label>
-        <input type='checkbox' checked={true} />
-        <input data-testid='modify-input' />
+        <input type='checkbox' checked={todos.isCompleted} />
+        <input data-testid='modify-input' value={todos.todo} />
       </label>
       <button data-testid='submit-button'>제출</button>
-      <button data-testid='cancel-button'>취소</button>
+      <button
+        data-testid='cancel-button'
+        onClick={() => handleIsEditing(todos.id)}
+      >
+        취소
+      </button>
     </>
   );
 };
